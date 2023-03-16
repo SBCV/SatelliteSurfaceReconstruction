802ed60904907ee450395b9bc770e0afb8d791d2
Valentin Wagner <wagner-valentin@gmx.de>
2023-03-16 11:04:14 +0100

    remove test trying delaunay meshing with sparse reconstruction

--- ssr/mvs_utility/colmap/colmap_mvs.py
+++ ssr/mvs_utility/colmap/colmap_mvs.py
@@ -51,9 +51,6 @@ class ColmapMVSReconstructor:
         if not os.path.isfile(mesh_ply_ofp) or not lazy:
             os.environ["PATH"] += os.pathsep + self.colmap_exe_dp
 
-            # TODO REMOVE THIS DEBUG LINE
-            reconstruction_idp = os.path.join(reconstruction_idp, "sparse")
-
             dense_mesher_call = [
                 "colmap",
                 "delaunay_mesher",
@@ -62,7 +59,7 @@ class ColmapMVSReconstructor:
                 "--output_path",
                 mesh_ply_ofp,
                 "--input_type",
-                "sparse",
+                "dense",
             ]
 
             if max_proj_dist is not None:
