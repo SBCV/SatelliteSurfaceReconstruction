1c45334ce2cbe47bf35f408e82df45b07482235b
Sebastian Bullinger <sebastian.bullinger@iosb.fraunhofer.de>
2023-10-31 23:48:57 +0100

    Update DFC preparation script

--- dataset_preparation/prepare_dfc2019_data.py
+++ dataset_preparation/prepare_dfc2019_data.py
@@ -5,21 +5,14 @@ from collections import defaultdict
 from ssr.utility.os_extension import get_file_paths_in_dir, mkdir_safely
 
 
-def _get_location_id_str(tif_ifp):
-    return os.path.basename(tif_ifp)[0:7]
-
-
-if __name__ == "__main__":
-
-    track_3_rgb_idp = "/path/to/Train-Track3-RGB-1/Track3-RGB-1"
-    track_3_truth_idp = "/path/to/Train-Track3-Truth/Track3-Truth"
-    root_odp = "/path/to/dataset_track3_prepared"
-
+def main(track_3_rgb_idp, track_3_truth_idp, root_odp):
     tif_ifp_list = get_file_paths_in_dir(track_3_rgb_idp, ext=".tif")
-
+    # Sites are defined by 3 letters followed by 3 digits (e.g. JAX_004)
     location_id_str_to_tif_list = defaultdict(list)
     for tif_ifp in tif_ifp_list:
-        location_id_str = _get_location_id_str(tif_ifp)
+        fn = os.path.basename(tif_ifp)
+        location_id_str = fn[0:7]
+        assert fn[7] == "_"
         location_id_str_to_tif_list[location_id_str].append(tif_ifp)
 
     mkdir_safely(root_odp)
@@ -39,3 +32,12 @@ if __name__ == "__main__":
             tif_fn = os.path.basename(tif_ifp)
             tif_ofp = os.path.join(odp, tif_fn)
             shutil.copyfile(tif_ifp, tif_ofp)
+
+
+if __name__ == "__main__":
+
+    track_3_rgb_idp = "/path/to/Train-Track3-RGB-1/Track3-RGB-1"
+    track_3_truth_idp = "/path/to/Train-Track3-Truth/Track3-Truth"
+    root_odp = "/path/to/dataset_track3_prepared"
+
+    main(track_3_rgb_idp, track_3_truth_idp, root_odp)
