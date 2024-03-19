6d76f3956dac704c0ebd138d111fff168c689a63
Sebastian <sebastian.bullinger@iosb.fraunhofer.de>
2024-03-19 18:54:16 +0100

    Add black required version

--- reformat_code.sh
+++ reformat_code.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Go to the directory where the script is located
 cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd
-black --line-length 79 ssr
-black --line-length 79 dataset_preparation
+REQUIRED_VERSION="24.3.0"
+black --required-version ${REQUIRED_VERSION} --line-length 79 ssr
+black --required-version ${REQUIRED_VERSION} --line-length 79 dataset_preparation
