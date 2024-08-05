1b2f7c372c1f8a75d0d441c2789fc076cd7c680e
Sebastian Bullinger <sebastian.bullinger@iosb.fraunhofer.de>
2024-08-05 17:42:18 +0200

    undo

--- ssr/config/ssr_config.py
+++ ssr/config/ssr_config.py
@@ -59,8 +59,8 @@ class SSRConfig(BaseModel):
 
     reconstruct_sfm_mvs: Union[bool, int]
     run_input_adapter: Union[bool, int]
-    extract_msi_pan_image_pairs: Union[bool, int] = False
-    use_consistent_msi_pan_extraction: Union[bool, int] = True
+    extract_msi: Union[bool, int] = False
+    extract_pan: Union[bool, int] = False
     pan_sharpening: Union[bool, int] = False
     depth_map_recovery: Union[bool, int]
     skew_correction: Union[bool, int]
